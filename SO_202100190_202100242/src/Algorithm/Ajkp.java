3b65036858b4ef3f69b87acb3a72c532b6581b57
Laura Costa <202100242@estudantes.ips.pt>
2023-01-17 00:37:24 +0000

    FileLoader e um pouco do Main

---
+++ SO_202100190_202100242/src/Algorithm/Ajkp.java
@@ -0,0 +1,5 @@
+package Algorithm;
+
+public class Ajkp extends Thread {
+
+}
--- SO_202100190_202100242/src/Main.java
+++ SO_202100190_202100242/src/Main.java
@@ -1,5 +1,20 @@
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+import Algorithm.Ajkp;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws FileNotFoundException, InterruptedException {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter file name:");
+        String fileName = scanner.nextLine();
+
+        System.out.println("Enter number of threads");
+        int threads = Integer.parseInt(scanner.nextLine());
+
+        System.out.println("Enter number of seconds");
+        int seconds = Integer.parseInt(scanner.nextLine());
+
     }
 }
